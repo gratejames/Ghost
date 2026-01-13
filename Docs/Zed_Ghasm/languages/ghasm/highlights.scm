f12e35bc9f60d92ad89b78eb3b199ef5d40f7418
GrateJames <jimmy.l.smythe@gmail.com>
2026-01-13 11:37:07 -0700

    Zed Support

---
+++ Docs/Zed_Ghasm/languages/ghasm/highlights.scm
@@ -0,0 +1,20 @@
+(string) @string
+(char) @string
+(number) @number
+(comment) @comment
+
+(register) @number
+
+(address) @operator
+
+(mne) @keyword.control
+
+(control
+  name: (control_name) @keyword.control)
+
+
+(label) @function
+
+(identifier) @variable
+
+(data) @constant
